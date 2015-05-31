e53aca81d4d1917b62b99556a255a430fbbb64b1
vishaldhaduk <vishal.dhaduk@hotmail.com>
2015-05-31 14:11:22 -0400

    Utility project added

---
+++ UtilityMaster/AppCore/MoneyConversionExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityMaster.AppCore
+{
+    class MoneyConversionExample
+    {
+        public MoneyConversionExample(decimal amount)
+        {
+            Amount = amount;
+        }
+        public decimal Amount { get; set; }
+
+        public static implicit operator decimal(MoneyConversionExample m)
+        {
+            return m.Amount;
+        }
+
+        public static explicit operator int(MoneyConversionExample m)
+        {
+            return (int)m.Amount;
+        }
+    }
+}
---
+++ UtilityMaster/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityMaster.AppCore;
+
+namespace UtilityMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallMoneyTypeConversion();
+            Console.ReadKey();
+        }
+
+        public static void CallMoneyTypeConversion()
+        {
+            MoneyConversionExample m = new MoneyConversionExample(30.5m);
+            decimal d = m;
+            int truncatedVAlue = (int)m;
+
+            Console.WriteLine("Decimal :" + d);
+            Console.WriteLine("Int :" + truncatedVAlue);
+
+            
+        }
+    }
+}
